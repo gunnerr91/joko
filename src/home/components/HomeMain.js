f44a4f831999b403ee75c73811777144854f2f36
gunnerr91 <blissvortex@hotmail.com>
2020-08-09 11:12:21 +0100

    initial commit, setting up navbar

---
+++ src/home/components/HomeMain.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function HomeMain() {
+  return (
+    <>
+      <div>Other part of the application</div>
+    </>
+  );
+}
+
+export default HomeMain;
---
+++ src/navbar/components/NavbarMain.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "../styles/NavbarMain.css";
+
+function NavbarMain() {
+  return (
+    <>
+      <div id="navbar">
+        <div id="logo">placeholder logo here</div>
+        <div id="links">
+          <button>how it works</button>
+          <button>buy</button>
+          <button>sell</button>
+          <button>sign up</button>
+          <button>account</button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default NavbarMain;
