c8211c5bb9b4594ce2c3044cf9c4a6fc4b51c725
gunnerr91 <blissvortex@hotmail.com>
2020-10-26 07:02:08 +0000

    removed header picture

--- src/home/components/HomeMain.js
+++ src/home/components/HomeMain.js
@@ -1,10 +1,8 @@
 import React from "react";
-import HomeHeaderPicture from "./HomeHeaderPicture";
 
 const HomeMain = () => {
   return (
     <>
-      <HomeHeaderPicture />
       <div>Other part of the application</div>
     </>
   );
